cc7b4e2c35217e1f9ce6d23edafb7aec4fb224ef
igi64 <igi64@yahoo.com>
2014-04-08 20:21:13 +0200

    tests

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var config = require('./server/config/config');
-var koaConfig = require('./server/config/koa');
+var configKoa = require('./server/config/koa');
 var koa = require('koa');
 var co = require('co');
 
@@ -10,12 +10,12 @@ var app = koa();
 module.exports = app;
 
 app.init = co(function *() {
-// koa config
-  koaConfig(app);
+// config koa
+  configKoa(app);
 
   // create http server and start listening for requests
   app.server = app.listen(config.server.http_port);
-  console.log('KOAN listening on port ' + config.server.http_port);
+  console.log('AiryHub listening on port ' + config.server.http_port);
 });
 
 app.use(function *() {
--- server/config/koa.js
+++ server/config/koa.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var logger = require('koa-logger');
 var config = require('./config');
 
 module.exports = function (app) {
   // middleware configuration
-
+  if (config.env !== 'test') {
+    app.use(logger());
+  }
 };
